88122da3323c772777e811a932cfe6f48894098e
MakenaMwirigi <makenasharon46@gmail.com>
2025-05-21 11:40:34 +0300

    JS DOMContentloaded-event assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+console.log("Before DOMContentLoaded listener");
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("The DOM has loaded");
+  const paragraph = document.getElementById("text");
+  paragraph.textContent = "This is really cool!";
+});
